7fde79037e2ea33b6cf3dff597dac5db0837b130
MinSu-Kim <net94.teacher73@gmail.com>
2018-11-24 15:22:29 +0900

    종료 수정

--- src/main/java/database_setting/Main.java
+++ src/main/java/database_setting/Main.java
@@ -1,10 +1,7 @@
 package database_setting;
 
 import java.awt.EventQueue;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
-import database_setting.jdbc.MyDataSource;
 import database_setting.ui.SettingUi;
 
 public class Main {
@@ -14,12 +11,6 @@ public class Main {
 			public void run() {
 				try {
 					SettingUi frame = new SettingUi();
-					frame.addWindowListener(new WindowAdapter() {
-						@Override
-						public void windowClosing(WindowEvent e) {
-							MyDataSource.getInstance().close();
-						}
-					});
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
